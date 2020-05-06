2fa61b8215d02a34f01c7dd23cc7acf7e5d0bffb
Mavriki <55285178+MavrikiDev@users.noreply.github.com>
2020-05-06 20:24:44 -0300

    Se crea una estructura de plantilla.
    Creé un esqueleto muy básico de plantilla, a efectos de crear la estructura general del MVC.

---
+++ vistas/plantilla.php
@@ -0,0 +1,20 @@
+<?php
+<!DOCTYPE html>
+<head>
+  <title><?php echo SERVERNAME ?></title>
+  
+</head>
+<body>
+  <?php
+  if(isset($_SESSION['inicioSesion']){
+    //Credencial válida, cargamos el sistema.
+  }
+  else{
+    //Debe logearse.
+  
+  }
+  ?>
+
+</body>
+</html>
+?>
